ab98a6f8c77ae5e820de2268f8f896b97d231a5c
sajal <sajalku442@gmail.com>
2025-11-12 23:54:11 +0530

    test push

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -8,6 +8,9 @@ export const COMPANY_API_END_POINT =
   "https://job-portal-api-ykfk.onrender.com/api/company";
 export const APPLICATION_API_END_POINT =
   "https://job-portal-api-ykfk.onrender.com/api/application";
+
+//Local
+
 // export const USER_API_END_POINT = "http://localhost:8000/api/user";
 // export const JOB_API_END_POINT = "http://localhost:8000/api/job";
 // export const JOB_APPLY_END_POINT = "http://localhost:8000/api/application";
